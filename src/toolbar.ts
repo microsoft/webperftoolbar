14d30db09d28322b8915bca92140ddbdc8e04cfc
Adam Reineke <adamre@microsoft.com>
2017-12-13 18:57:36 -0800

    Try to set up initial rendering (tests failing)

--- src/toolbar.ts
+++ src/toolbar.ts
@@ -2,12 +2,31 @@
 /** Describes the toolbar. */
 export class Toolbar
 {
+    private root: HTMLElement;
+
     /**
      * Creates the toolbar.
      * @param panels The panels to be displayed when the toolbar is opened.
+     * @param container Optional parameter that defaults to the body of the HTML page.
      */
-    constructor(panels: Panel[])
+    constructor(private panels: Panel[], container: HTMLElement = window.document.body)
     {
+        this.root = document.createElement("div");
+        container.appendChild(this.root);
+    }
 
+    public render(container: HTMLElement): void
+    {
+        var out: string[] = new Array(4);
+        out.push("<ul>");
+        for(let p of this.panels)
+        {
+            for(let b of p.getButtons())
+            {
+                out.push(`<li style="background-color:{$b}">${b.emoji} ${b.getValue()}</li>`);
+            }
+        }
+        out.push("</ul>");
+        container.innerHTML = out.join("");
     }
 }
--- test/toolbar.spec.ts
+++ test/toolbar.spec.ts
@@ -1,3 +1,11 @@
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import 'mocha';
+import { Toolbar } from '../src/toolbar';
 
+describe('Toolbar class', () => {
+    it('should construct with no panels', () => {
+        const container = document.createElement("div");
+        const toolbar = new Toolbar([], container);
+        assert.equal(1, container.childElementCount);
+    });
+});
