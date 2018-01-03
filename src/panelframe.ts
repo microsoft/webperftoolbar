baebdcfcc2f4899cfabd623fbc048b70e13ee4c3
Adam Reineke <adamre@microsoft.com>
2018-01-02 18:46:51 -0800

    Some initial styling

--- src/panelframe.ts
+++ src/panelframe.ts
@@ -11,24 +11,29 @@ export class PanelFrame {
     /** Tracks the visible state of the frame. */
     private isVisible: boolean;
 
+    /** The root of the toolbar */
+    private toolbarRoot: HTMLElement;
+
     /**
      * Creates the panel frame.
      * @param toolbarRoot The DOM element to contain the frame. Should be the root of the toolbar.
      */
     public constructor(toolbarRoot: HTMLElement) {
+        this.toolbarRoot = toolbarRoot;
+        this.isVisible = false;
+
         this.frame = document.createElement("div");
         this.frame.setAttribute("id", "PTB_frame");
-        toolbarRoot.appendChild(this.frame);
 
-        this.isVisible = false;
     }
 
     /** Show the provided panel, or hide the displayed panel. */
     public toggle(panel: IPanel): void {
-        if (this.isVisible) {
-            this.frame.innerHTML = "";
-        } else {
+        if (!this.isVisible || this.frame.parentNode === null) {
             panel.render(this.frame);
+            this.toolbarRoot.appendChild(this.frame);
+        } else {
+            this.frame.parentNode.removeChild(this.frame);
         }
 
         this.isVisible = !this.isVisible;
