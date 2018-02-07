1a2c4eae02e66427782f7787b94c0ec291f64df9
Adam Reineke <adamre@microsoft.com>
2018-02-07 14:32:20 -0800

    Preserve copyright headers, include GitHub link and version in output

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,18 @@
 const merge = require('webpack-merge');
 const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common');
+const package = require('./package.json');
 
 module.exports = merge(common, {
     plugins: [
-        new UglifyJsWebpackPlugin()
+        new UglifyJsWebpackPlugin({
+            uglifyOptions: {
+                output: {
+                    preamble: "/* https://github.com/Microsoft/WebPerfToolbar v" + package.version + " */",
+                    // Don't let uglify remove important comments
+                    comments: /copyright|license|preserve|\/\*\!/gi,
+                },
+            },
+        })
     ]
 });
