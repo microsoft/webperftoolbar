a6680c8cd67bf008a6a191665b012000dbd1c711
Adam Reineke <adamre@microsoft.com>
2018-08-27 16:38:50 -0700

    Basic info buttons

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export { Toolbar } from "./toolbar";
 export { Button } from "./button";
 export { NavigationTimingsPanel } from "./panels/navigation-timing";
 export { ResourceTimingsPanel } from "./panels/resource-timing";
+export { BasicInfoPanel } from "./panels/basic-info";
